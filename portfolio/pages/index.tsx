e934d62ad5d0ead7800218c8a3b87ce8fd3f05fd
Dragiinus <melvinmaute@gmail.com>
2023-04-13 23:33:39 +0200

    BackgroundCircles

--- portfolio/pages/index.tsx
+++ portfolio/pages/index.tsx
@@ -1,16 +1,23 @@
+import Head from 'next/head'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+      <Head>
         <title>Portfolio de Melvin</title>
+      </Head>
 
         <Header />
   
         {/* Hero */}
+        <section id="hero">
+          <Hero />
+        </section>
 
         {/* About */}
 

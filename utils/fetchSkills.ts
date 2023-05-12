615941e13e3436d477d462d286c815fcf40ec292
Dragiinus <melvinmaute@gmail.com>
2023-05-12 04:13:00 +0200

    test

--- utils/fetchSkills.ts
+++ utils/fetchSkills.ts
@@ -2,7 +2,7 @@ import { Skill } from "@/typings";
 
 export const fetchSkills = async () => {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSkills`
+        `https://portfolio-dragiinus.vercel.app/api/getSkills`
     );
 
     const data = await res.json()
--- utils/fetchSocials.ts
+++ utils/fetchSocials.ts
@@ -2,7 +2,7 @@ import { Social } from "@/typings";
 
 export const fetchSocial = async () => {
     const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/getSocials`
+        `https://portfolio-dragiinus.vercel.app/api/getSocials`
     );
 
     const data = await res.json()
